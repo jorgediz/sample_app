cc26ed8bbe389d0c862b36c16d329db39eec611a
Jorge Diz <jorge.a.diz@gmail.com>
2012-04-22 14:03:42 -0300

    Done with Exercise 1 of chapter 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -45,4 +45,19 @@ describe "StaticPages" do
         :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+    it "should have the header 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector(
+        'h1',
+        :text => 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector(      
+        'title',
+        :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
